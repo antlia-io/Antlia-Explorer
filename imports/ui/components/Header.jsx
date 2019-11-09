4ba00fe13e390b866dba24a349fbfb723cc899af
aniqmir <aniqmir@gmail.com>
2019-11-09 12:41:46 +0500

    transaction, to field hidden on submit proposal type

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -1,110 +1,153 @@
-import React, { Component } from 'react';
-import { HTTP } from 'meteor/http'
-import { Badge, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import SearchBar from './SearchBar.jsx';
-import i18n from 'meteor/universe:i18n';
-import ToggleMenu from './ToggleMenu.jsx'
+import React, { Component } from "react";
+import { HTTP } from "meteor/http";
+import {
+  Badge,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import SearchBar from "./SearchBar.jsx";
+import i18n from "meteor/universe:i18n";
+import ToggleMenu from "./ToggleMenu.jsx";
 
 const T = i18n.createComponent();
 export default class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            network: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      network: ""
+    };
+  }
+
+  handleLanguageSwitch = (lang, e) => {
+    i18n.setLocale(lang);
+  };
+
+  componentDidMount() {
+    let url = Meteor.settings.public.networks;
+    if (!url) return;
+    try {
+      HTTP.get(url, null, (error, result) => {
+        if (result.statusCode == 200) {
+          let networks = JSON.parse(result.content);
+          if (networks.length > 0) {
+            this.setState({
+              networks: (
+                <DropdownMenu>
+                  {networks.map((network, i) => {
+                    return (
+                      <span key={i}>
+                        <DropdownItem header>
+                          <img src={network.logo} /> {network.name}
+                        </DropdownItem>
+                        {network.links.map((link, k) => {
+                          return (
+                            <DropdownItem
+                              key={k}
+                              disabled={
+                                link.chain_id == Meteor.settings.public.chainId
+                              }
+                            >
+                              <a href={link.url} target="_blank">
+                                {link.chain_id}{" "}
+                                <Badge size="xs" color="secondary">
+                                  {link.name}
+                                </Badge>
+                              </a>
+                            </DropdownItem>
+                          );
+                        })}
+                        {i < networks.length - 1 ? (
+                          <DropdownItem divider />
+                        ) : (
+                          ""
+                        )}
+                      </span>
+                    );
+                  })}
+                </DropdownMenu>
+              )
+            });
+          }
         }
+      });
+    } catch (e) {
+      console.warn(e);
     }
+  }
 
-    handleLanguageSwitch = (lang, e) => {
-        i18n.setLocale(lang)
-    }
+  render() {
+    return (
+      <Navbar className="background" dark expand="lg" fixed="top" id="header">
+        <ToggleMenu className="displayres" />
 
-    componentDidMount() {
-
-
-        let url = Meteor.settings.public.networks
-        if (!url) return
-        try {
-            HTTP.get(url, null, (error, result) => {
-                if (result.statusCode == 200) {
-                    let networks = JSON.parse(result.content);
-                    if (networks.length > 0) {
-                        this.setState({
-                            networks: <DropdownMenu>{
-                                networks.map((network, i) => {
-                                    return <span key={i}>
-                                        <DropdownItem header><img src={network.logo} /> {network.name}</DropdownItem>
-                                        {network.links.map((link, k) => {
-                                            return <DropdownItem key={k} disabled={link.chain_id == Meteor.settings.public.chainId}>
-                                                <a href={link.url} target="_blank">{link.chain_id} <Badge size="xs" color="secondary">{link.name}</Badge></a>
-                                            </DropdownItem>
-                                        })}
-                                        {(i < networks.length - 1) ? <DropdownItem divider /> : ''}
-                                    </span>
-
-                                })
-                            }</DropdownMenu>
-                        })
-                    }
-                }
-            })
-        }
-        catch (e) {
-            console.warn(e);
-        }
-    }
-
-    render() {
-        return (
-            <Navbar className="background" dark expand="lg" fixed="top" id="header">
-
-                <ToggleMenu className="displayres"/>
-
-                <NavLink className="navbar-brand" to="/"><img src="/img/antlia.svg" className="img-fluid logo" /></NavLink>
-                {/* <UncontrolledDropdown className="d-inline text-nowrap">
+        <NavLink className="navbar-brand" to="/">
+          <img src="/img/antlia.svg" className="img-fluid logo" />
+        </NavLink>
+        {/* <UncontrolledDropdown className="d-inline text-nowrap">
                     <DropdownToggle caret={(this.state.networks !== "")} tag="span" size="sm" id="network-nav">{Meteor.settings.public.chainId}</DropdownToggle>
                     {this.state.networks}
                 </UncontrolledDropdown> */}
-                <div className="rightbar">
+        <div className="rightbar">
+          <SearchBar
+            id="header-search"
+            className="form-control"
+            history={this.props.history}
+          />
 
-                    <SearchBar id="header-search" className="form-control" history={this.props.history} />
+          <UncontrolledDropdown inNavbar>
+            <DropdownToggle nav caret>
+              {Meteor.settings.public.chainId}
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>Cosmoshub2</DropdownItem>
+              <DropdownItem>Cosmoshub2</DropdownItem>
+              <DropdownItem>Cosmoshub2</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
 
-
-                    <UncontrolledDropdown inNavbar>
-                        <DropdownToggle nav caret>
-                            {Meteor.settings.public.chainId}
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem>Cosmoshub2</DropdownItem>
-                            <DropdownItem>Cosmoshub2</DropdownItem>
-                            <DropdownItem>Cosmoshub2</DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-
-                    <UncontrolledDropdown inNavbar>
-                        <DropdownToggle nav caret>
-                            <T>navbar.lang</T>
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem onClick={(e) => this.handleLanguageSwitch('en-US', e)}><T>navbar.english</T></DropdownItem>
-                            <DropdownItem onClick={(e) => this.handleLanguageSwitch('zh-Hant', e)}><T>navbar.chinese</T></DropdownItem>
-                            <DropdownItem onClick={(e) => this.handleLanguageSwitch('zh-Hans', e)}><T>navbar.simChinese</T></DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                    <UncontrolledDropdown inNavbar>
-                        <DropdownToggle nav caret>
-                            <img src="/img/user.png" className="img-fluid usericon" />
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem>User Profile</DropdownItem>
-                            <DropdownItem>Views</DropdownItem>
-                            <DropdownItem>Sign Out</DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                </div>
-
-
-            </Navbar>
-        );
-    }
+          <UncontrolledDropdown inNavbar>
+            <DropdownToggle nav caret>
+              <T>navbar.lang</T>
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem
+                onClick={e => this.handleLanguageSwitch("en-US", e)}
+              >
+                <T>navbar.english</T>
+              </DropdownItem>
+              <DropdownItem
+                onClick={e => this.handleLanguageSwitch("zh-Hant", e)}
+              >
+                <T>navbar.chinese</T>
+              </DropdownItem>
+              <DropdownItem
+                onClick={e => this.handleLanguageSwitch("zh-Hans", e)}
+              >
+                <T>navbar.simChinese</T>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+          <UncontrolledDropdown inNavbar>
+            <DropdownToggle nav caret>
+              <img src="/img/user.png" className="img-fluid usericon" />
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>User Profile</DropdownItem>
+              <DropdownItem>Views</DropdownItem>
+              <DropdownItem>Sign Out</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </div>
+      </Navbar>
+    );
+  }
 }
--- imports/ui/home/Transactions/TransactionRow.jsx
+++ imports/ui/home/Transactions/TransactionRow.jsx
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, Card, Alert, Spinner, Button } from 'reactstrap';
-import { TxIcon } from '../../components/Icons.jsx';
-import Activities from '../../components/Activities.jsx';
-import ColorErrors from '../../components/ColorErrors.jsx';
-import ButtonActivities from '../../components/ButtonActivities.jsx';
-import TimeAgo from '../../components/TimeAgo.jsx';
-import numbro from 'numbro';
-import moment from 'moment';
-import ScrollArea from 'react-scrollbar';
-import Coin from '/both/utils/coins.js'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Card, Alert, Spinner, Button } from "reactstrap";
+import { TxIcon } from "../../components/Icons.jsx";
+import Activities from "../../components/Activities.jsx";
+import ColorErrors from "../../components/ColorErrors.jsx";
+import ButtonActivities from "../../components/ButtonActivities.jsx";
+import TimeAgo from "../../components/TimeAgo.jsx";
+import numbro from "numbro";
+import moment from "moment";
+import ScrollArea from "react-scrollbar";
+import Coin from "/both/utils/coins.js";
 // import TransactionActivities from './TransactionActivities.jsx';
 
-export const TransactionRow = (props) => {
-    let tx = props.tx;
-   let txTags = props.tx.tags
-   console.log(tx)
-   let from = txTags[1].value;
-   let to = txTags[2].value
-//    console.log(from)
-//    console.log(to)
-    // console.log(tx);
-    return (
-        <div>
-            {/* <Row className={(tx.code) ? "tx-info invalid" : "tx-info"}>
+export const TransactionRow = props => {
+  let tx = props.tx;
+  let txTags = props.tx.tags;
+  console.log(tx.tx.value.msg[0].type);
+  let from = txTags[1].value;
+  let to = txTags[2].value;
+  //    console.log(from)
+  //    console.log(to)
+  // console.log(tx);
+  return (
+    <div>
+      {/* <Row className={(tx.code) ? "tx-info invalid" : "tx-info"}>
         {(!props.blockList) ? 
         <Col xs={5} md={2}>{(tx.tx.value.msg && tx.tx.value.msg.length > 0) ? tx.tx.value.msg.map((msg) => {
             return <p><ButtonActivities msg={msg} invalid={(!!tx.code)} tags={tx.tags} /></p>
@@ -59,32 +59,53 @@ export const TransactionRow = (props) => {
 
     </Row>  */}
 
-            {/* <div className={(tx.code) ? "tx-info invalid" : "tx-info", "transaction-item"}> */}
-            <div className="transaction-item">
-                <div className="trans-img">
-                    <img src="/img/user.png" />
-                </div>
-                <div className="trans-details">
-
-                    <div className="tc"> <Link to={"/transactions/" + tx.txhash} className="txlink">{tx.txhash}</Link></div>
-                    {/* <p className="tc">cosmosdjowur9874u8ejiif8e90uf43809ujfiofj489rueoos...</p> */}
-                    <div className="from-to">
-                        {/* <TransactionActivities/> */}
-                        <p className="from">From: <Link to="#">{from}</Link>...</p>
-                        <p>To: <Link to="#">{to}</Link>...</p>
-                    </div>
-                </div>
-                <div className="action">
-                    {(!props.blockList) ? <div> {(tx.tx.value.msg && tx.tx.value.msg.length > 0) ? tx.tx.value.msg.map((msg) => {
-                        return <ButtonActivities msg={msg} invalid={(!!tx.code)} />
-                    }) : ''} </div>: ''}
-                    <TimeAgo time={tx.block().time} />
-                </div>
-                <div className="status">
-                    <Button>{(!tx.code) ? <TxIcon valid /> : <TxIcon />}</Button>
-                </div>
-            </div>
-
+      {/* <div className={(tx.code) ? "tx-info invalid" : "tx-info", "transaction-item"}> */}
+      <div className="transaction-item">
+        <div className="trans-img">
+          <img src="/img/user.png" />
         </div>
-    )
-}
+        <div className="trans-details">
+          <div className="tc">
+            {" "}
+            <Link to={"/transactions/" + tx.txhash} className="txlink">
+              {tx.txhash}
+            </Link>
+          </div>
+          {/* <p className="tc">cosmosdjowur9874u8ejiif8e90uf43809ujfiofj489rueoos...</p> */}
+          <div className="from-to">
+            {/* <TransactionActivities/> */}
+            <p className="from">
+              From: <Link to="#">{from}</Link>...
+            </p>
+            <p>
+              {tx.tx.value.msg[0].type === "cosmos-sdk/MsgSubmitProposal" ? (
+                <React.Fragment></React.Fragment>
+              ) : (
+                <React.Fragment>
+                  To: <Link to="#">{to}</Link>...
+                </React.Fragment>
+              )}
+            </p>
+          </div>
+        </div>
+        <div className="action">
+          {!props.blockList ? (
+            <div>
+              {tx.tx.value.msg && tx.tx.value.msg.length > 0
+                ? tx.tx.value.msg.map(msg => {
+                    return <ButtonActivities msg={msg} invalid={!!tx.code} />;
+                  })
+                : ""}
+            </div>
+          ) : (
+            ""
+          )}
+          <TimeAgo time={tx.block().time} />
+        </div>
+        <div className="status">
+          <Button>{!tx.code ? <TxIcon valid /> : <TxIcon />}</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
